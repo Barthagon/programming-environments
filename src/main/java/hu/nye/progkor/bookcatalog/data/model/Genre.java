c58321132c364436f82e7b83e9c0da2b316a3744
simon <simon.bence45@gmail.com>
2023-05-20 08:22:03 +0200

    Implemented book-catalog webapp

---
+++ src/main/java/hu/nye/progkor/bookcatalog/data/model/Genre.java
@@ -0,0 +1,22 @@
+package hu.nye.progkor.bookcatalog.data.model;
+
+public enum Genre {
+    Action,
+    Adventure,
+    Biography,
+    Children,
+    Comedy,
+    Crime,
+    Drama,
+    Fantasy,
+    HistoricalFiction,
+    Horror,
+    Mystery,
+    NonFiction,
+    Poetry,
+    Romance,
+    ScienceFiction,
+    SelfHelp,
+    Thriller,
+    YoungAdult
+}
---
+++ src/test/java/hu/nye/progkor/bookcatalog/BookCatalogApplicationTests.java
@@ -0,0 +1,24 @@
+package hu.nye.progkor.bookcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void testMainMethod() {
+		// Mock or set up any necessary dependencies or test data
+
+		// Call the main method
+		BookCatalogApplication.main(new String[] {});
+
+		// Add assertions or checks to verify the expected behavior or output
+		// of your application
+	}
+
+}
